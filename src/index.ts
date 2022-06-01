f4482f7bfafe115a22d841e74b6006b753aaefd1
Martin Rossil <martinrossil@gmail.com>
2022-06-01 11:23:14 +0200

    Testing export * for docs creation.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-export { Product } from '@martinrossil/domain';
+/* export { Product } from '@martinrossil/domain';
 export { ProductInterface } from '@martinrossil/domain';
 export { COMPONENTS } from '@martinrossil/components';
 export { Component } from '@martinrossil/components';
-export { ComponentInterface } from '@martinrossil/components';
+export { ComponentInterface } from '@martinrossil/components'; */
+
+export * from '@martinrossil/domain';
+export * from '@martinrossil/components';
